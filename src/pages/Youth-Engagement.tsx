e53cd0bd984662a1ac694387d95a698ed924988c
BSIT606J2022 <116421244+BSIT606J2022@users.noreply.github.com>
2025-05-26 16:32:17 +0300

    adding youth empowerment header section

--- src/pages/Youth-Engagement.tsx
+++ src/pages/Youth-Engagement.tsx
@@ -1,18 +1,33 @@
-
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-
 
 const YouthEngagement = () => {
   return (
     <>
       <Navbar />
       <main>
-        <h4>
-             Youth Engagement and Empowerment
-            </h4> 
-         
+        <section className="pt-20 pb-16 md:pt-32 md:pb-24 bg-gray-50">
+          <div className="container-custom">
+            <div className="text-center max-w-3xl mx-auto">
+              <h1 className="text-4xl md:text-5xl font-bold mb-6">
+                Youth and
+                <span className="text-gradient-blue">
+                  {" "}
+                  Community Engagement
+                </span>{" "}
+                Program
+              </h1>
+              <p className="text-xl text-gray-700">
+                Youth empowerment and social transformation by fostering an
+                environment where young people can express their ideas, engage
+                in decision-making processes, and actively contribute to their
+                communities.
+              </p>
+            </div>
+          </div>
+        </section>
       </main>
+
       <Footer />
     </>
   );
